ce6082bb50cf8b016464254fb67d7b6da08955d4
Aristides <aristides.neto3@aluno.ce.gov.br>
2025-11-27 17:39:36 +0000

    finalizado

--- index.js
+++ index.js
@@ -3,23 +3,32 @@ const prompt = require("prompt-sync")()
 function verificaIdade(idade){
     if(idade > 17){
         console.log("Atleta fora de idade!")
-    }else if(idade <= 14){
-        console.log("Atleta não tem a idade idade adequada!")
-    }else{
+    } else if(idade <= 14){
+        console.log("Atleta não tem a idade adequada!")
+    } else {
         console.log("Você tem a idade adequada para jogar o campeonato")
     }
 }
 
-let atletas = {}
-let atleta = [atletas]
+let atletas = []  
 
 console.log("Bem vindo ao cadastro de atletas do campeonato sub 17 de futsal")
-console.log("Nesse cadastro você tera que colocar o nome do atleta a sua idade e a posição que joga ")
+console.log("Nesse cadastro você tera que colocar o nome do atleta, a sua idade e a posição que joga.")
+let nomeEquipe = prompt("Digite o nome da equipe:")
+for (let i = 0; i < 6; i++) {
 
-for(let i = 0; i < 6; i++ ){
-[atleta[0].`nome${i}`] = prompt("Digite o nome do atleta: ")
-atletas.idade = parseInt(prompt("Digite a idade do atleta: "))
-atletas.posicao = prompt("Digite a posição que o atleta joga: ")
-verificaIdade(atletas.idade)
-console.log(atletas)
+    let atleta = {}   
+
+    atleta.nome = prompt(`Digite o nome do ${i+1}º atleta: `)
+    atleta.idade = parseInt(prompt("Digite a idade do atleta: "))
+    atleta.posicao = prompt("Digite a posição que o atleta joga: ")
+
+    verificaIdade(atleta.idade)
+
+    atletas.push(atleta) 
+    console.log(atleta)
 }
+
+console.log("\nLista completa de atletas cadastrados:")
+console.log(atletas)
+console.log(`${nomeEquipe}`)
