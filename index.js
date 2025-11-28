6eb51e203fd9fc868c52e2feef1e315ac4fbbb30
Aristides <aristides.neto3@aluno.ce.gov.br>
2025-11-28 16:49:06 +0000

    finalizado!!

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const prompt = require("prompt-sync")()
 
 function verificaIdade(idade){
     if(idade > 17){
-        console.log("Atleta fora de idade!")
+        console.log("Atleta fora de idade! escolha um jogador mais jovem.")
     } else if(idade <= 14){
-        console.log("Atleta não tem a idade adequada!")
+        console.log("Atleta não tem a idade adequada para jogar o campeonato!")
     } else {
-        console.log("Você tem a idade adequada para jogar o campeonato")
+        console.log("Atleta tem a idade adequada para jogar o campeonato")
     }
 }
 
@@ -20,8 +20,8 @@ for (let i = 0; i < 7; i++) {
     let atleta = {}   
 
     atleta.nome = prompt(`Digite o nome do ${i+1}º atleta: `)
-    atleta.idade = parseInt(prompt("Digite a idade do atleta: "))
-    atleta.posicao = prompt("Digite a posição que o atleta joga: ")
+    atleta.idade = parseInt(prompt(`Digite a idade do ${i+1}° atleta: `))
+    atleta.posicao = prompt(`Digite a posição que o ${i+1}° atleta joga: `)
 
     verificaIdade(atleta.idade)
 
